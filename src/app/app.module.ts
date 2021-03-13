3f1431d8694b974db89b3e4c721b6854636609dd
davidecarizzoni <davidecarizzoni@gmail.com>
2021-03-13 11:46:45 +0100

    add login and register page with base style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AngularFireDatabaseModule} from '@angular/fire/database';
 import { environment} from '../environments/environment';
 import { AngularFirestoreModule} from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // import { LoginModule } from './features/login/login.module';
 
 @NgModule({
@@ -22,7 +23,8 @@ import { CommonModule } from '@angular/common';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    NgbModule
 
   ],
   providers: [],
